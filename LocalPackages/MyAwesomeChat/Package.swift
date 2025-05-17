2b549208a3e1ebbc64482a85d578a3f1f56492fc
mivafoxy <creatox1996@gmail.com>
2025-05-17 12:43:48 +0300

    Починилки проекта

--- LocalPackages/MyAwesomeChat/Package.swift
+++ LocalPackages/MyAwesomeChat/Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "MyAwesomeChat",
+    defaultLocalization: "ru",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyAwesomeChat",
             targets: ["MyAwesomeChat"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyAwesomeChat"),
+            name: "MyAwesomeChat",
+            dependencies: ["SnapKit"]
+        ),
         .testTarget(
             name: "MyAwesomeChatTests",
             dependencies: ["MyAwesomeChat"]
